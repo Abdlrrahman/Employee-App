577aae1a57f56d3a654dddac10200994872f46fa
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-06-02 00:55:25 +0200

    First message

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Is this the real life? Is this just fantasy?
+      Caught in a landslide, no escape from reality
+      Open your eyes, look up to the skies and see
+      I'm just a poor boy, I need no sympathy
+      Because I'm easy come, easy go, little high, little low
+      Any way the wind blows doesn't really matter to me, to me
+      </Text>
     </View>
   );
 }
