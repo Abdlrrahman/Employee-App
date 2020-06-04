3f30b9dd759534a4eeb26b33eb42ec06d06248b7
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-06-04 14:12:19 +0200

    Style view

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 import { Card } from 'react-native-paper'
 
 
 export default function Home() {
     return (
         <Card style={styles.myCard}>
-            <Text style={{ fontSize: 22 }}>
-                Welcome Home
+            <View style={styles.myCard}>
+                <Image style={{ width: 60, height: 60, borderLeftWidth: 30 }}
+                    source={{ uri: "https://images.unsplash.com/photo-1591121213541-c93b6e74c47a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80" }}
+                />
+                <Text>
+                    Welcome Home
             </Text>
+            </View>
         </Card>
     );
 }
@@ -16,6 +21,8 @@ export default function Home() {
 const styles = StyleSheet.create({
     myCard: {
         margin: 5,
-        padding: 5,
     },
+    cardView: {
+        flexDirection: "row",
+    }
 });
