a70acce39cb5d210fc851dd0cdf72a5f05c82cb1
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-06-05 15:52:01 +0200

    CreateEmployee TextInput Name

---
+++ screens/CreateEmployee.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { TextInput } from 'react-native-paper'
+
+
+export default function CreateEmployee() {
+    const [Name, setName] = useState("")
+    const [Phone, setPhone] = useState("")
+    const [Email, setEmail] = useState("")
+    const [Salary, setSalary] = useState("")
+    const [Picture, setPicture] = useState("")
+    const [Model, setModel] = useState(false)
+
+    return (
+        <View style={styles.root}>
+            <TextInput style={styles.inputStyle} label='Name' value={Name} mode="outlined" theme={theme} onChangeText={text => setName(text)} />
+        </View>
+    )
+}
+
+const theme = {
+    colors: {
+        primary: "red"
+    }
+}
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+    },
+    inputStyle: {
+        margin: 5,
+    }
+});
