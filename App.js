92441376e7f8432120c0f33c61980902c8fbfd4a
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-06-06 01:10:07 +0200

    Profile

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Contants from 'expo-constants'
 // import Home from './screens/Home'
 // import CreateEmployee from './screens/CreateEmployee'
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 
@@ -7,6 +7,9 @@ export default function Profile() {
     return (
         <View style={styles.root}>
             <LinearGradient colors={["#0033ff", "#6bc1ff"]} style={{ height: "20%" }} />
+            <View style={{ alignItems: "center" }}>
+                <Image style={{ width: 100, height: 100, borderRadius: 50, marginTop: -50 }} source={{ uri: "https://images.unsplash.com/photo-1542909192-2f2241a99c9d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" }} />
+            </View>
         </View>
     );
 }
