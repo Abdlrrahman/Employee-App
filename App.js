4f39b45ab331b6db1c2cb5bff0da8277a0a01d98
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-06-07 09:00:24 +0200

    app Navigation

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Contants from 'expo-constants'
-// import Home from './screens/Home'
-// import CreateEmployee from './screens/CreateEmployee'
+import Home from './screens/Home'
+import CreateEmployee from './screens/CreateEmployee'
 import Profile from './screens/Profile'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Home /> */}
-      {/* <CreateEmployee /> */}
-      <Profile />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="CreateEmployee" component={CreateEmployee} />
+          <Stack.Screen name="Profile" component={Profile} />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
