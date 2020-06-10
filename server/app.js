95328fabe4425b166846f78ffce3bcca9e0ea550
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-06-10 16:45:13 +0200

    app js

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const port = 3000
+
+const mongodb = `mongodb+srv://Abdo:${process.env.DB_PASSWORD}@cluster0-u4hb7.mongodb.net/<dbname>?retryWrites=true&w=majority`
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Listening at ${port}`))
