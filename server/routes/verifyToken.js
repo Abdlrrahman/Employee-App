51607bd6dc1c6dfaf0676380e9ee7850a9352535
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-06-22 19:19:05 +0200

    next

---
+++ server/routes/verifyToken.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+    const token = req.header("auth-token");
+
+    if (!token) return res.status(401).send("Access Denied");
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+        req.employer = verified;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Invalid Token");
+    }
+}
