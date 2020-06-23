e0c9e1379e953f2244ff1011bdb9e9e1c20e0b2e
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-06-23 21:53:07 +0200

    redisServer

--- server/services/generateAccessToken.js
+++ server/services/generateAccessToken.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 const JWTR = require('jwt-redis').default;
-const redisClient = redis.createClient();
-const jwtr = new JWTR(redisClient);
+// const redisClient = redis.createClient();
+// const jwtr = new JWTR(redisClient);
 
 const generateAccessToken = (user) => {
     return jwtr.sign(user, process.env.TOKEN_SECRET, { expiresIn: '10m' })
--- server/services/generateRefreshToken.js
+++ server/services/generateRefreshToken.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 const JWTR = require('jwt-redis').default;
-const redisClient = redis.createClient();
-const jwtr = new JWTR(redisClient);
+// const redisClient = redis.createClient();
+// const jwtr = new JWTR(redisClient);
 
 const generateRefreshToken = (user) => {
     return jwtr.sign(user, process.env.REFRESH_TOKEN)
